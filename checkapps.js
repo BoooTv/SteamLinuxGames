78d09d02327277b1aa9018382ee28f3b96b31545
Huxley <huxhales@gmail.com>
2019-11-07 17:27:27 +1000

    I broke it

--- checkapps.js
+++ checkapps.js
@@ -1,42 +1,64 @@
 const request = require("request");
-let GAMES = {};
+const fs = require("fs");
 
 async function start() {
-  // let apps = await getAppID();
-  let apps = [440, 730];
-  await apps.forEach(appid => {
-    check(appid);
-  });
+  let apps = await getAppID();
+  console.log(apps);
+  // let apps = [440, 730];
+  // apps.forEach(appid => {
+  //   check(appid);
+  // });
   // await console.log(apps.applist.apps);
-  // await apps.applist.apps.forEach(app => {
+  await apps.applist.apps.forEach(app => {
+    check(app["appid"]);
+    console.log(app);
+  });
   // console.log(app);
   // check(app.appid);
   // });
 }
 
-async function check(id) {
-  await request(
+function writeGAMES(id) {
+  fs.readFile("GAMES.json", "utf8", function readFileCallback(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      obj = JSON.parse(data); //now it an object
+      obj[id] = true; //add some data
+      json = JSON.stringify(obj); //convert it back to json
+      fs.writeFile("GAMES.json", json, "utf8", err => {
+        if (err) {
+          console.log(err);
+        }
+      }); // write it back
+    }
+  });
+}
+
+function check(id) {
+  console.log("Checking id: " + id);
+  request(
     "https://store.steampowered.com/api/appdetails?appids=" + id,
     (err, response, body) => {
+      // console.log(response);
       if (!err) {
         let res = JSON.parse(body);
         if (typeof res === "object") {
           // console.log(body);
           // console.log(res[id]);
           if (res[id].success === true) {
-            let id = id;
-            let linux = res[id].data.platform.linux;
+            let linux = res[id].data.platforms.linux;
             if (linux === true) {
-              GAMES.push({ id: linux });
               console.log(id + " is supported on linux");
+              writeGAMES(id);
             }
           }
         } else {
-          console.log("Something went wrong");
+          console.log("Something went wrong. res not json");
           process.exit(1);
         }
       } else {
-        console.log("Something went wrong");
+        console.log("Something went wrong: " + err);
       }
     }
   );
